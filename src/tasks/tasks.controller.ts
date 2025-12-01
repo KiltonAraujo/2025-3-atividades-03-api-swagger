9fe62322584ff7950b2be204da6ceae97b6a0631
Kilton J. Araújo <kilton.araujo@gmail.com>
2025-12-01 18:20:53 +0000

    documentado o endpoint raiz

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,25 +1,30 @@
 import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Put,
-  Param,
-  Delete,
-  ParseIntPipe,
-  HttpCode,
-  HttpStatus,
+  Controller, Get,
+  Post, Body,
+  Put, Param,
+  Delete, ParseIntPipe,
+  HttpCode, HttpStatus,
 } from '@nestjs/common';
+
+import {
+  ApiTags,
+  ApiOperation,
+  ApiResponse,
+} from '@nestjs/swagger'; 
+
 import { TasksService } from './tasks.service';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
 import { Task } from './task.entity';
 
 @Controller('tasks')
+@ApiTags('root')
 export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Obter todas as tarefas', description: 'geta as tarefas cadastradas})' })
+  @ApiResponse({ status: 200, description: 'ta bala patrao.' })
   findAll(): Promise<Task[]> {
     return this.tasksService.findAll();
   }

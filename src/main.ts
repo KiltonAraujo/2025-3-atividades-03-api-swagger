4add6ffe1f9d762bfaa5281b398defae32bdfc59
Kilton J. Araújo <kilton.araujo@gmail.com>
2025-12-01 17:51:06 +0000

    configurado o ambiente swagger

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Habilitar CORS
   app.enableCors({
-    origin: true, // Em produção, especifique domínios específicos
+    origin: true,
     credentials: true,
   });
 
-  // Habilitar validação global
   app.useGlobalPipes(new ValidationPipe({
     transform: true,
     whitelist: true,
     forbidNonWhitelisted: true,
   }));
+
+  const config = new DocumentBuilder()
+    .setTitle('API de Tarefinhas (TODO List)')
+    .setDescription('API para gerenciamento de tarefas da turma Infoweb 2025')
+    .setVersion('1.0')
+    .addTag('tasks')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api-docs', app, document);
 
   const port = process.env.PORT || 3000;
   await app.listen(port);

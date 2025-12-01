c79f79d62f37f0eb45496af8fa4fb1b65b14a425
Kilton J. Araújo <kilton.araujo@gmail.com>
2025-12-01 18:09:43 +0000

    documentado as entidades

--- src/tasks/task.entity.ts
+++ src/tasks/task.entity.ts
@@ -6,6 +6,8 @@ import {
   UpdateDateColumn,
 } from 'typeorm';
 
+import { ApiProperty } from '@nestjs/swagger';
+
 export enum TaskStatus {
   ABERTO = 'aberto',
   FAZENDO = 'fazendo',
@@ -15,12 +17,15 @@ export enum TaskStatus {
 @Entity()
 export class Task {
   @PrimaryGeneratedColumn()
+  @ApiProperty()
   id: number;
 
   @Column()
+  @ApiProperty()
   title: string;
 
   @Column()
+  @ApiProperty()
   description: string;
 
   @Column({
@@ -31,8 +36,10 @@ export class Task {
   status: TaskStatus;
 
   @CreateDateColumn()
+  @ApiProperty()
   createdAt: Date;
 
   @UpdateDateColumn()
+  @ApiProperty()
   updatedAt: Date;
 }
